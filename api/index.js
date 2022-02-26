a65f4a341b86998e6b3a83494c2804b1070ac4bc
jonathanqwong <jonathan@Jonathans-MacBook-Pro.local>
2022-02-25 18:39:31 -0800

    separate server from client commands

--- api/index.js
+++ api/index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3001
+const client_port = process.env.PORT
 const redis = require('redis');
 const client = redis.createClient();
 
 app.get('/api/jobs', async (req, res) => {
     await client.connect();
     const jobs = await client.get('serpapi')
-    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:3000");
+    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:${client_port}");
     await client.disconnect();
     return res.send(jobs);
 })
