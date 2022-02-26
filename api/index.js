ac0ffcfda5678d41bbaa0f391666051e8d15a414
jonathanqwong <jonathan@Jonathans-MacBook-Pro.local>
2022-02-25 18:48:24 -0800

    build client and server

--- api/index.js
+++ api/index.js
@@ -1,14 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3001
-const client_port = process.env.PORT
+const port = process.env.PORT || 3001
 const redis = require('redis');
 const client = redis.createClient();
 
 app.get('/api/jobs', async (req, res) => {
     await client.connect();
     const jobs = await client.get('serpapi')
-    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:${client_port}");
+    res.header("Access-Control-Allow-Origin", "http://0.0.0.0:${port}");
     await client.disconnect();
     return res.send(jobs);
 })
