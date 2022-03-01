064a04eea4211c673db0a13f8ae0419074ba5e3b
jonathanqwong <jonathan@Jonathans-MacBook-Pro.local>
2022-02-28 22:42:08 -0800

    connecto to heroku redis add on

--- worker/tasks/fetch-jobs.js
+++ worker/tasks/fetch-jobs.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 const redis = require('redis');
-const client = redis.createClient({url: process.env.REDIS_IP});
+const client = redis.createClient(process.env.REDIS_URL, {no_ready_check: true});
 const BASE_URL = 'https://serpapi.com/search';
 const headers = {'Content-Type': 'application/json'};
 const params = {
