8e3dcb17d61ac3b2a37232e4099f0d3347299ee5
jonathanqwong <jonathan@Jonathans-MacBook-Pro.local>
2022-03-29 13:31:02 -0700

    set port in express and listen function

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
+app.set('port', (process.env.PORT || 5000));
+
 const redis = require('redis');
 const client = redis.createClient({url: process.env.REDIS_URL});
 const app = express();
@@ -11,16 +13,18 @@ app.get('/api/jobs', async (req, res) => {
     res.header("Access-Control-Allow-Origin", `https://0.0.0.0:${port}`);
     await client.disconnect();
     return res.send(jobs);
-})
+}).listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
 
 // // Have Node serve the files for our built React app
 // app.use(express.static('client'));
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 
-app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`)
+// })
 
 // All other GET requests not handled before will return our React app
 app.get('*', (req, res) => {
